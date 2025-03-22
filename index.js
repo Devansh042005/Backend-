47384afea521c593f9b585e860bf4d7f68a80d0e
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-03-23 00:53:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-// express.js ---> EK NPM package hai , It is a framework
-// framework gives u flow but u have things available 
-// express js manages everything from receiving the request and giving the response
+const express  =require('express');
+const app = express();
+const path = require('path');
 
-const express = require('express')
-const app = express() // express ki poori powers app mei h
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine','ejs');// using this we can render the ejs routes
+app.use(express.static(path.join(__dirname ,'public')));
 
-// routes create krna
 
-app.get('/' , function(req  ,res){ // here the function is the request handler which is a middleware and the '/' is the route
-    res.send('Hello World')
-});
-
-app.get('/profile' , function(req  ,res){ // here the function is the request handler which is a middleware and the '/' is the route
-    res.send('champion uska coach')
-}); 
-
-app.get('/about', function(req, res){
-    res.send('Web3 X AI')
+app.get("/", function(req, res){
+    res.render("index");
 })
 
-app.listen(3000)
+app.get("/profile/:username",function(res,req){
+    res.send( `welcome ,${req.params.username}`);
+})
+
+app.get("/author/:username/:age",function(res,req){
+    res.send( `welcome ,${req.params.username}`);
+})
+
+app.listen(3000,function(){
+    console.log("its running");
+})
